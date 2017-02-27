671586c99286c74aa60110accc5c5d21be719e75
koolay <koolay@163.com>
2017-02-28 01:23:44 +0800

    config

--- cmd/root.go
+++ cmd/root.go
@@ -18,8 +18,11 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/koolay/econfig/config"
 	"github.com/spf13/cobra"
+	jww "github.com/spf13/jwalterweatherman"
+	"github.com/spf13/viper"
 )
 
 // RootCmd represents the base command when called without any subcommands
@@ -33,6 +36,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		initializeConfig()
 	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
@@ -64,4 +68,39 @@ func init() {
 // initConfig reads in config file and ENV variables if set.
 func preInit() {
 
+}
+
+func initLogging(verbose bool) {
+	jww.SetLogFile("econfig.log")
+	if verbose {
+		jww.SetLogThreshold(jww.LevelTrace)
+		jww.SetStdoutThreshold(jww.LevelInfo)
+	} else {
+		jww.SetLogThreshold(jww.LevelTrace)
+		jww.SetStdoutThreshold(jww.LevelError)
+	}
+}
+
+func initializeConfig() {
+	initLogging(config.GlobalFlags.Verbose)
+	viper.SetConfigType("toml")
+	if config.GlobalFlags.CfgFile != "" {
+		// enable ability to specify config file via flag
+		viper.SetConfigFile(config.GlobalFlags.CfgFile)
+	}
+
+	jww.INFO.Printf("verbose: %v", config.GlobalFlags.Verbose)
+	viper.SetConfigName(".econfig") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")    // adding home directory as first search path
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv() // read in environment variables that match
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 }
--- main.go
+++ main.go
@@ -15,50 +15,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fsnotify/fsnotify"
 	"github.com/koolay/econfig/cmd"
-	"github.com/koolay/econfig/config"
-	jww "github.com/spf13/jwalterweatherman"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	initializeConfig()
 	cmd.Execute()
-}
-
-func initLogging(verbose bool) {
-	jww.SetLogFile("econfig.log")
-	if verbose {
-		jww.SetLogThreshold(jww.LevelTrace)
-		jww.SetStdoutThreshold(jww.LevelInfo)
-	} else {
-		jww.SetLogThreshold(jww.LevelTrace)
-		jww.SetStdoutThreshold(jww.LevelError)
-	}
-}
-
-func initializeConfig() {
-	initLogging(config.GlobalFlags.Verbose)
-	viper.SetConfigType("toml")
-	if config.GlobalFlags.CfgFile != "" {
-		// enable ability to specify config file via flag
-		viper.SetConfigFile(config.GlobalFlags.CfgFile)
-	}
-
-	jww.INFO.Printf("verbose: %v", config.GlobalFlags.Verbose)
-	viper.SetConfigName(".econfig") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")    // adding home directory as first search path
-	viper.AddConfigPath(".")
-	viper.AutomaticEnv() // read in environment variables that match
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("Config file changed:", e.Name)
-	})
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	} else {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
 }
